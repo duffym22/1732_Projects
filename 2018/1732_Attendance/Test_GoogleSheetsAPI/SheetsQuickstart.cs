3b1be1c733a9580e4d327a9aa0c017c75812aa55
Matthew Duffy <duffym22@gmail.com>
2018-10-25 23:07:26 -0500

    Successfully reads from Google Sheet - need to fix writing/appending data to spreadsheet

--- 2018/1732_Attendance/Test_GoogleSheetsAPI/SheetsQuickstart.cs
+++ 2018/1732_Attendance/Test_GoogleSheetsAPI/SheetsQuickstart.cs
@@ -42,22 +42,22 @@ namespace Test_GoogleSheetsAPI
             });
 
             // Define request parameters.
-            String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
-            String range = "Class Data!A2:E";
+            String spreadsheetId = "13U-gYgtXlh8Q0Qgaim6nzrFlkOAP4dJP2hvOTaO7nTg";
+            String range = "ATTENDANCE!A2:C";
             SpreadsheetsResource.ValuesResource.GetRequest request =
                     service.Spreadsheets.Values.Get(spreadsheetId, range);
 
             // Prints the names and majors of students in a sample spreadsheet:
-            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            // https://docs.google.com/spreadsheets/d/13U-gYgtXlh8Q0Qgaim6nzrFlkOAP4dJP2hvOTaO7nTg/edit
             ValueRange response = request.Execute();
             IList<IList<Object>> values = response.Values;
             if (values != null && values.Count > 0)
             {
-                Console.WriteLine("Name, Major");
+                Console.WriteLine("ID, Timestamp, Status");
                 foreach (var row in values)
                 {
                     // Print columns A and E, which correspond to indices 0 and 4.
-                    Console.WriteLine("{0}, {1}", row[0], row[4]);
+                    Console.WriteLine("{0}, {1}, {2}", row[0], row[1], row[2]);
                 }
             }
             else
@@ -65,6 +65,17 @@ namespace Test_GoogleSheetsAPI
                 Console.WriteLine("No data found.");
             }
             Console.Read();
+
+            IList<IList<object>> appRows = new List<IList<object>>();
+            IList <object> vals = new List<object>() { "2222", DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss"), "IN" };
+            appRows.Add(vals);
+
+            ValueRange updateRange = new ValueRange();
+            updateRange.Values = appRows;
+            SpreadsheetsResource.ValuesResource.AppendRequest oopDat = 
+                service.Spreadsheets.Values.Append(updateRange, spreadsheetId, range);
+
+            AppendValuesResponse resp = oopDat.Execute();
         }
     }
 }
