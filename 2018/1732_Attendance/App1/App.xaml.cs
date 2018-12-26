978c7aa1cfc029d30af3b3a43c6c5f0f66dbb8f3
Matthew Duffy <duffym22@gmail.com>
2018-12-26 12:18:31 -0600

    - Added new UWP project to try deploying to Raspberry Pi

---
+++ 2018/1732_Attendance/App1/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml;
+
+namespace _1732_Attendance
+{
+   /// <summary>
+   /// Interaction logic for App.xaml
+   /// </summary>
+   public partial class App : Application
+    {
+    }
+}
